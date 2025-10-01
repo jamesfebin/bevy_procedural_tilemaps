32ec151a06de79fdcf41e31df942514df9fdf098
Febin John James <jamesfebin@gmail.com>
2025-10-01 21:41:53 +0530

    Adjusting exports

--- bevy_procedural_tilemaps/src/prelude.rs
+++ bevy_procedural_tilemaps/src/prelude.rs
@@ -1,8 +1,7 @@
 //! Commonly used Bevy helpers re-exported for convenience.
 
+pub use crate::assets::{BundleInserter, ModelAsset, ModelsAssets};
 pub use crate::proc_gen::prelude::*;
-pub use crate::{
-    assets::{BundleInserter, ModelAsset, ModelsAssets},
-    simple_plugin::ProcGenSimplePlugin,
-    spawner::NodesSpawner,
-};
+#[cfg(feature = "simple-plugin")]
+pub use crate::simple_plugin::ProcGenSimplePlugin;
+pub use crate::spawner::NodesSpawner;
